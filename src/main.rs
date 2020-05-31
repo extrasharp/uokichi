1ff455281dc2b4ae61905d9d6d6d7b7e4a2304c5
extrasharp <genericpb@gmail.com>
2020-05-30 19:54:04 -0500

    compile

--- src/main.rs
+++ src/main.rs
@@ -58,18 +58,21 @@ impl Bits for u64 {}
 
 #[derive(Debug)]
 struct Opdef {
+    name: String,
     base: u64,
     args: Vec<u64>,
+    shift: i32,
 }
 
 impl Opdef {
-    fn new(spec_str: &str, arg_order: &str) -> Self {
+    fn new(name: &str, spec_str: &str, arg_order: &str, shift: i32) -> Self {
         let spec_bytes = spec_str.as_bytes();
         let arg_bytes = arg_order.as_bytes();
 
         let base = spec_bytes.iter()
-            .fold(0, | acc, byte | {
-                if *byte == '1' as u8 {
+            .map(| &byte | byte == '1' as u8)
+            .fold(0, | acc, tf | {
+                if tf {
                     (acc << 1) | 1
                 } else {
                     acc << 1
@@ -77,10 +80,11 @@ impl Opdef {
             });
 
         let args = arg_bytes.iter()
-            .map(| arg_byte | {
+            .map(| &arg_byte | {
                 spec_bytes.iter()
-                    .fold(0, | acc, spec_byte | {
-                        if *arg_byte == *spec_byte {
+                    .map(| &spec_byte | arg_byte == spec_byte)
+                    .fold(0, | acc, tf | {
+                        if tf {
                             (acc << 1) | 1
                         } else {
                             acc << 1
@@ -90,38 +94,23 @@ impl Opdef {
             .collect();
 
         Opdef {
+            name: name.to_string(),
             base,
             args,
+            shift,
         }
     }
 
     fn apply(&self, arg_vals: &[u64]) -> u64 {
         // TODO check arg_vals and args same len
-        arg_vals.iter()
-            .zip(self.args.iter())
-            .map(| (&arg, &mask) | mask.eat(arg))
-            .fold(self.base, | acc, x | acc | x)
-    }
-}
-
-// instructions are used because
-//   for jumps you need to know the location of all the opdefs
-//     before you generate the code for them
-
-#[derive(Debug)]
-struct Idef {
-    name: String,
-    opdef: Opdef,
-    shift: i32,
-}
-
-impl Idef {
-    fn apply(&self, args: &[u64]) -> u64 {
         if self.shift == 0 {
-            self.opdef.apply(&args)
+            self.args.iter()
+                .zip(arg_vals.iter())
+                .map(| (&mask, &arg) | mask.eat(arg))
+                .fold(self.base, | acc, x | acc | x)
         } else {
-            let arg = args[0] >> self.shift;
-            self.opdef.apply(&[arg])
+            let arg = arg_vals[0] >> self.shift;
+            self.base | self.args[0].eat(arg)
         }
     }
 }
@@ -138,7 +127,7 @@ enum HexRecordType {
     StarLinearAddress,
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 struct HexRecord {
     typ: HexRecordType,
     addr: u16,
@@ -153,12 +142,12 @@ impl HexRecord {
 
         let mut ret: u16 = 0;
 
-        ret = ret.wrapping_add(self.addr);
-        ret = ret.wrapping_add(self.typ as u16);
-        ret = ret.wrapping_add(self.addr >> 8);
         ret = ret.wrapping_add(self.data.len() as u16);
-        for byte in &self.data {
-            ret = ret.wrapping_add(*byte as u16)
+        ret = ret.wrapping_add(self.addr);
+        ret = ret.wrapping_add(self.addr >> 8);
+        ret = ret.wrapping_add(self.typ as u16);
+        for &byte in &self.data {
+            ret = ret.wrapping_add(byte as u16)
         }
 
         (0xff & twos_complement(ret)) as u8
@@ -180,6 +169,10 @@ impl Display for HexRecord {
 
 //
 
+// instructions are used because
+//   for jumps you need to know the location of all the opdefs
+//     before you generate the code for them
+
 #[derive(Debug)]
 enum IArg {
     Raw(u64),
@@ -190,10 +183,36 @@ enum IArg {
     }
 }
 
+impl IArg {
+    fn resolve(&self, label_table: &HashMap<String, u64>, addr: u64) -> Result<u64, CompileError> {
+        match self {
+            IArg::Raw(val) => Ok(*val),
+            IArg::LabelAccess {
+                name,
+                is_relative,
+                offset
+            } => {
+                if let Some(label_addr) = label_table.get(name) {
+                    if *is_relative {
+                        // TODO check this works
+                        Ok((*label_addr as i32
+                            - addr as i32
+                            + *offset) as u64)
+                    } else {
+                        Ok(*label_addr)
+                    }
+                } else {
+                    Err(CompileError::LabelNotFound(name.clone()))
+                }
+            },
+        }
+    }
+}
+
 #[derive(Debug)]
 enum CodeObject<'a> {
     Instruction {
-        idef: &'a Idef,
+        opdef: &'a Opdef,
         args: Vec<IArg>,
     },
     RawData(u64),
@@ -201,10 +220,31 @@ enum CodeObject<'a> {
     LabelTag(String),
 }
 
+fn generate_address_image(code_objs: &[CodeObject], mut offset: u64) -> Vec<u64> {
+    use CodeObject::*;
+
+    let mut addr_image = Vec::new();
+    addr_image.reserve(code_objs.len());
+
+    for i in 0..code_objs.len() {
+        addr_image.push(offset);
+        match code_objs[i] {
+            AddressTag(addr)             => offset = addr,
+            RawData(_) | Instruction{..} => offset += 1,
+            _ => {}
+        }
+    }
+
+    addr_image
+}
+
+
 //
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 struct CompileSettings {
+    opcode_size: u8,
+    address_size: u8,
     eof_record: HexRecord,
     words_per_record: u8,
 }
@@ -213,18 +253,13 @@ struct CompileSettings {
 enum CompileError {
     StartWithAddressTag,
     DuplicateLabel(String),
-}
-
-#[derive(Debug)]
-struct CodeInfo {
-    opcode_size: u8,
-    address_size: u8,
+    LabelNotFound(String),
 }
 
 #[derive(Debug)]
 struct Code<'a> {
-    info: CodeInfo,
     code: Vec<CodeObject<'a>>,
+    settings: CompileSettings,
     addr_image: Vec<u64>,
     label_table: HashMap<String, u64>,
 }
@@ -232,33 +267,17 @@ struct Code<'a> {
 // TODO check that code.len() doesnt wrapover type of A
 //       for the A::from(i).unwrap()
 impl<'a> Code<'a> {
-    fn new(info: CodeInfo, code: Vec<CodeObject<'a>>) -> Result<Self, CompileError> {
+    fn new(settings: CompileSettings, code: Vec<CodeObject<'a>>) -> Result<Self, CompileError> {
         use CodeObject::*;
 
-        let mut offset =
+        let offset =
             if let AddressTag(addr) = code[0] {
                 addr
             } else {
                 return Err(CompileError::StartWithAddressTag);
             };
 
-        let mut addr_image = Vec::new();
-        addr_image.reserve(code.len());
-
-        for i in 0..code.len() {
-            addr_image.push(offset);
-            match code[i] {
-                AddressTag(addr) => {
-                    offset = addr;
-                },
-                RawData(_) | Instruction{..} => {
-                    offset += 1;
-                },
-                _ => {}
-            }
-        }
-
-        let addr_image = addr_image;
+        let addr_image = generate_address_image(&code, offset);
         let mut label_table = HashMap::new();
 
         for (&addr, obj) in addr_image.iter().zip(code.iter()) {
@@ -271,11 +290,77 @@ impl<'a> Code<'a> {
         }
 
         Ok(Code {
-            info,
             code,
+            settings,
             addr_image,
             label_table,
         })
+    }
+
+    fn to_opcodes(&self) -> Result<Vec<u64>, CompileError> {
+        use CodeObject::*;
+
+        let ret: Result<Vec<_>, _> =
+            self.addr_image.iter()
+                           .zip(self.code.iter())
+                           .filter_map(| (&addr, obj) | {
+                               match obj {
+                                   RawData(val) => Some(Ok(*val)),
+                                   Instruction {
+                                       opdef,
+                                       args
+                                   } => {
+                                       let resolved: Result<Vec<_>, _> =
+                                           args.iter()
+                                               .map(| arg | arg.resolve(&self.label_table, addr))
+                                               .collect();
+                                       match resolved {
+                                           Ok(r_args) => Some(Ok(opdef.apply(&r_args))),
+                                           Err(err) => Some(Err(err))
+                                       }
+                                   },
+                                   _ => None
+                               }
+                           })
+                           .collect();
+
+        ret
+
+        /*
+        for (&addr, obj) in self.addr_image.iter().zip(self.code.iter()) {
+            match obj {
+                RawData(val) => ret.push(*val),
+                Instruction{ opdef, args } => {
+                    let mut resolved_args: Vec<u64> = Vec::new();
+                    resolved_args.reserve(args.len());
+
+                    for iarg in args {
+                        match iarg {
+                            IArg::Raw(val) => resolved_args.push(*val),
+                            IArg::LabelAccess{name, is_relative, offset} => {
+                                match self.label_table.get(name) {
+                                    Some(label_addr) => {
+                                        if *is_relative {
+                                            // TODO check this works
+                                            resolved_args.push((*label_addr as i32
+                                                                - addr as i32
+                                                                + *offset) as u64);
+                                        } else {
+                                            resolved_args.push(*label_addr);
+                                        }
+                                    },
+                                    None => return Err(CompileError::LabelNotFound(name.clone())),
+                                }
+                            }
+                        }
+                    }
+
+                    ret.push(opdef.apply(&resolved_args));
+                },
+                _ => {}
+            }
+        }
+        */
     }
 }
 
@@ -294,35 +379,37 @@ fn main() {
     // println!("{:08b}", opd.apply(&[0b01, 0b11]));
     // println!("{:08b}", opd.apply(&[0b10, 0b00]));
 
-    let idef_add = Idef {
-        name: "add".to_string(),
-        opdef: Opdef::new("0011aabb", "ab"),
-        shift: 0,
+    let opdef_add = Opdef::new("add", "0011aabb", "ab", 0);
+    let opdef_jmp = Opdef::new("jmp", "1100aaaa", "a", 8);
+
+    let settings = CompileSettings {
+        opcode_size: 8,
+        address_size: 16,
+        eof_record: HexRecord {
+            typ: HexRecordType::EndOfFile,
+            data: Vec::new(),
+            addr: 0,
+        },
+        words_per_record: 10
     };
 
-    let idef_jmp = Idef {
-        name: "jmp".to_string(),
-        opdef: Opdef::new("0011aaaa", "a"),
-        shift: 8,
-    };
-
-    let c = Code::new(CodeInfo{opcode_size: 8, address_size: 16},
+    let c = Code::new(settings.clone(),
         vec![
             CodeObject::AddressTag(0),
             CodeObject::LabelTag("start".to_string()),
-            CodeObject::Instruction{idef: &idef_add, args: vec![IArg::Raw(0b11), IArg::Raw(0b00)]},
-            CodeObject::Instruction{idef: &idef_jmp, args: vec![IArg::Raw(0xdead)]},
+            CodeObject::Instruction{opdef: &opdef_add, args: vec![IArg::Raw(0b11), IArg::Raw(0b00)]},
+            CodeObject::Instruction{opdef: &opdef_jmp, args: vec![IArg::Raw(0xdead)]},
             CodeObject::RawData(0xad),
         ]);
 
     println!("{:?}", c);
 
-    let c = Code::new(CodeInfo{opcode_size: 8, address_size: 16},
+    let c = Code::new(settings.clone(),
         vec![
             CodeObject::AddressTag(0),
             CodeObject::LabelTag("start".to_string()),
-            CodeObject::Instruction{idef: &idef_add, args: vec![IArg::Raw(0b11), IArg::Raw(0b00)]},
-            CodeObject::Instruction{idef: &idef_jmp, args: vec![IArg::Raw(0xdead)]},
+            CodeObject::Instruction{opdef: &opdef_add, args: vec![IArg::Raw(0b11), IArg::Raw(0b00)]},
+            CodeObject::Instruction{opdef: &opdef_jmp, args: vec![IArg::Raw(0xdead)]},
             CodeObject::LabelTag("another".to_string()),
             CodeObject::RawData(0xad),
         ]);
